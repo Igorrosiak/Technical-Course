c41b11a11f835a4e08fb21cf2c5fd9d9a69ed019
Igorrosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-03-10 23:05:01 -0300

    1

--- receita 24.02 atualizado/untitled/src/br/com/senai/Main.java
+++ receita 24.02 atualizado/untitled/src/br/com/senai/Main.java
@@ -1,4 +1,16 @@
 package br.com.senai;
 
+import br.com.senai.model.Receita;
+
 public class Main {
+    public static void main (String [] args){
+        Receita receita1 = new Receita();
+        receita1.setNome("Bolo de chocolate");
+        receita1.setDescricao("Bolo da vovó");
+        receita1.setIngrediente("chocolate", 200, "g");
+        receita1.setIngrediente("farinha", 500, "g");
+        receita1.setIngrediente("ovos", 3, "unidade");
+        receita1.setIngrediente("Açúcar", 2, "xícaras");
+        System.out.println(receita1);
+    }
 }
