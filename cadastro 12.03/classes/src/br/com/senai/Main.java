29194658fe273181193468e1a50fa5a6963f6886
Igorrosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-03-12 20:03:31 -0300

    1

--- cadastro 12.03/classes/src/br/com/senai/Main.java
+++ cadastro 12.03/classes/src/br/com/senai/Main.java
@@ -1,8 +1,18 @@
 package br.com.senai;
 
+import br.com.senai.model.Usuario;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Usuario igor = new Usuario();
+        igor.setNome("Igor Luiz Belter Rosiak");
+        igor.setEmail("Igorluizbelterrosiak@gmail.com");
+        igor.setTelefone("48-9-8842-5374");
+        igor.setEndereço("Braulina Machado, 51");
+        igor.setCpf("129.078.329.22");
+
+        System.out.println(igor);
     }
 }
--- cadastro 12.03/classes/src/br/com/senai/model/Usuario.java
+++ cadastro 12.03/classes/src/br/com/senai/model/Usuario.java
@@ -5,12 +5,16 @@ public class Usuario {
     private String nome;
     private String email;
     private String telefone;
+    private String endereço;
+    private String cpf;
 
     //construtor
-    public Usuario(String n, String e, String t){
-        this.nome = n;
-        this.email = e;
-        this.telefone = t;
+    public Usuario(){
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereço = endereço;
+        this.cpf = cpf;
     }
 
     //getter and setter
@@ -31,5 +35,27 @@ public class Usuario {
     }
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+    public String getEndereço() {
+        return endereço;
+    }
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "\nUsuario:\n" +
+                "\nNome: " + nome +
+                "\nEmail: " + email +
+                "\nTelefone: " + telefone +
+                "\nEndereço: " + endereço +
+                "\nCpf: " + cpf;
     }
 }
