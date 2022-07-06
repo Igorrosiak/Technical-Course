258a9a31e4289b361feb1bfd4a6a34b3dd996d56
Igorrosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-07-06 09:27:35 -0300

    Creating new Activity

---
+++ 05 06.07 Usuario-Restful/src/main/java/br/senai/model/Usuario.java
@@ -0,0 +1,83 @@
+package br.senai.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 50)
+    private String nome;
+
+    @NotNull
+    @Size(max = 60)
+    private String username;
+
+    @Size(max = 255)
+    @NotNull
+    private String senha;
+
+    private Date dataNasc;
+
+    @Size(max = 100)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario: " +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", username='" + username + '\'' +
+                ", senha='" + senha + '\'' +
+                ", dataNasc=" + dataNasc +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
